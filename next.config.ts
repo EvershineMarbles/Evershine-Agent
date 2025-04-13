9550598063cce35a50cc7942973dfa3c93a15743
Sonal Baheti <sonal.21mcan176@jecrcu.edu.in>
2025-04-14 04:04:48 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,12 @@
 const nextConfig = {
   // Disable TypeScript type checking during build
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
   // Disable ESLint during build
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  // Other Next.js config options can go here
 }
 
 module.exports = nextConfig
