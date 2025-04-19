eb99994fc9135cb99ca1c056d5030bf60f9d3936
Sonal Baheti <sonal.21mcan176@jecrcu.edu.in>
2025-04-19 12:49:27 +0530

    update

--- src/components/icon-sidebar.tsx
+++ src/components/icon-sidebar.tsx
@@ -4,7 +4,6 @@ import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { cn } from "@/lib/utils"
 import { Home, ShoppingCart, Heart, QrCode, Package, Settings, LogOut, User } from "lucide-react"
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
 
 interface IconSidebarProps {
   clientId: string
@@ -48,49 +47,29 @@ export function IconSidebar({ clientId }: IconSidebarProps) {
 
   return (
     <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-dark text-white shadow-lg">
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <div className="sidebar-icon mt-4">
-              <User size={24} />
-            </div>
-          </TooltipTrigger>
-        </Tooltip>
-      </TooltipProvider>
+      <div className="sidebar-icon mt-4 relative group">
+        <User size={24} />
+      </div>
 
       <hr className="sidebar-hr my-2" />
 
-      <TooltipProvider>
-        {routes.map((route) => (
-          <Tooltip key={route.href}>
-            <TooltipTrigger asChild>
-              <Link href={route.href} className={cn("sidebar-icon", pathname === route.href ? "bg-blue" : "")}>
-                <route.icon size={24} />
-                <span className="sidebar-tooltip z-[9999]">{route.name}</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right" className="z-[9999]">
-              {route.name}
-            </TooltipContent>
-          </Tooltip>
-        ))}
-      </TooltipProvider>
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn("sidebar-icon relative group", pathname === route.href ? "bg-blue" : "")}
+        >
+          <route.icon size={24} />
+          <div className="tooltip-wrapper">{route.name}</div>
+        </Link>
+      ))}
 
       <div className="mt-auto mb-4">
         <hr className="sidebar-hr my-2" />
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link href="/" className="sidebar-icon">
-                <LogOut size={24} />
-                <span className="sidebar-tooltip z-[9999]">Logout</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right" className="z-[9999]">
-              Logout
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+        <Link href="/" className="sidebar-icon relative group">
+          <LogOut size={24} />
+          <div className="tooltip-wrapper">Logout</div>
+        </Link>
       </div>
     </div>
   )
