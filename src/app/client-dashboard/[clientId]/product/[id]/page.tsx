6d43d4ae7dc2f23e7376a628f7488cf23524cdaa
Sonal Baheti <sonal.21mcan176@jecrcu.edu.in>
2025-05-27 09:30:21 +0530

    sd

--- src/app/client-dashboard/[clientId]/product/[id]/page.tsx
+++ src/app/client-dashboard/[clientId]/product/[id]/page.tsx
@@ -600,7 +600,7 @@ export default function ProductDetail() {
               </Button>
             </div>
 
-            <div className="space-y-4">
+            <div className="mt-4 space-y-4">
               <div>
                 <Label htmlFor="customQuantity">Quantity (sqft)</Label>
                 <Input
